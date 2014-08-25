0540739d162a610ed4797238dc726be278b877cd
Chris Mackey <chris@mackeyarchitecture.com>
2014-08-25 00:26:55 -0400

    Turned Preview Back on For U Object

--- src/Ladybug_Wind Rose.py
+++ src/Ladybug_Wind Rose.py
@@ -40,7 +40,7 @@ Provided by Ladybug 0.0.58
 
 ghenv.Component.Name = "Ladybug_Wind Rose"
 ghenv.Component.NickName = 'windRose'
-ghenv.Component.Message = 'VER 0.0.58\nAUG_22_2014'
+ghenv.Component.Message = 'VER 0.0.58\nAUG_23_2014'
 ghenv.Component.Category = "Ladybug"
 ghenv.Component.SubCategory = "2 | VisualizeWeatherData"
 #compatibleLBVersion = VER 0.0.58\nAUG_20_2014
