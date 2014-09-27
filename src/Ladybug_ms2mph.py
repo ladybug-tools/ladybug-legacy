3cbdf15eea66d3c74b67b7d96dc7586ef33837de
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2014-09-27 14:38:02 -0400

    Added to Ladybug
    http://www.grasshopper3d.com/xn/detail/2985220:Comment:1138247

---
+++ src/Ladybug_ms2mph.py
@@ -0,0 +1,35 @@
+#C to F
+# By Mostapha Sadeghipour Roudsari
+# Sadeghipour@gmail.com
+# Ladybug started by Mostapha Sadeghipour Roudsari is licensed
+# under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+
+"""
+Convert from m/s to mile/h 
+-
+Provided by Ladybug 0.0.49
+    
+    Args:
+        ms: Input wind speed in meters per second
+    Returns:
+        mph: Output wind speed in miles per hour
+"""
+
+ghenv.Component.Name = "Ladybug_ms2mph"
+ghenv.Component.NickName = 'ms2mph'
+ghenv.Component.Message = 'VER 0.0.58\nSEP_27_2014'
+ghenv.Component.Category = "Ladybug"
+ghenv.Component.SubCategory = "4 | Extra"
+#compatibleLBVersion = VER 0.0.58\nAUG_20_2014
+try: ghenv.Component.AdditionalHelpFromDocStrings = "4"
+except: pass
+
+def convertTomph(ms):
+    return ms*2.23694
+    
+mph = []
+for num in ms:
+   if num == 'm/s': mph.append('mph')
+   else:
+       try: mph.append(convertTomph(num))
+       except: mph.append(num)
