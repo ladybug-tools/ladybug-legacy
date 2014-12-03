21c9f2e2e4e548ffe42697eb2d565754eb17c3c7
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2014-12-02 22:19:30 -0500

    Fixed version compatibility
    @chriswmackey let's update compatibleVersion line once you update
    functions inside Ladybug_ladybug to avoid version incompatibilities. Do
    you know that you literally have written more line for Ladybug than I!
    :) Check contributors.

--- src/Ladybug_3D Chart.py
+++ src/Ladybug_3D Chart.py
@@ -32,10 +32,10 @@ Provided by Ladybug 0.0.58
 
 ghenv.Component.Name = "Ladybug_3D Chart"
 ghenv.Component.NickName = '3DChart'
-ghenv.Component.Message = 'VER 0.0.58\nNOV_16_2014'
+ghenv.Component.Message = 'VER 0.0.58\nDEC_02_2014'
 ghenv.Component.Category = "Ladybug"
 ghenv.Component.SubCategory = "2 | VisualizeWeatherData"
-#compatibleLBVersion = VER 0.0.58\nAUG_20_2014
+#compatibleLBVersion = VER 0.0.58\nDEC_02_2014
 try: ghenv.Component.AdditionalHelpFromDocStrings = "1"
 except: pass
 
