be512a570467bc55a9b9d5ae30ed12e15b6f712c
Chris Mackey <chris@mackeyarchitecture.com>
2015-05-26 21:43:29 -0400

    Added Djordje's User Object Binary
    This makes the merging of @stgeorges 's recent work complete.

--- src/ladybug_ladybug.py
+++ src/ladybug_ladybug.py
@@ -29,7 +29,7 @@ Provided by Ladybug 0.0.59
 
 ghenv.Component.Name = "Ladybug_Ladybug"
 ghenv.Component.NickName = 'Ladybug'
-ghenv.Component.Message = 'VER 0.0.59\nAPR_21_2015'
+ghenv.Component.Message = 'VER 0.0.59\nMAY_26_2015'
 ghenv.Component.Category = "Ladybug"
 ghenv.Component.SubCategory = "0 | Ladybug"
 try: ghenv.Component.AdditionalHelpFromDocStrings = "1"
