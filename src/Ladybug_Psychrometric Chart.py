2ed18051905ead3ea571816d022d02a234d3fc92
Chris Mackey <chris@mackeyarchitecture.com>
2016-02-19 22:37:52 -0500

    Fixed a Small but Important Bug

--- src/Ladybug_Psychrometric Chart.py
+++ src/Ladybug_Psychrometric Chart.py
@@ -90,7 +90,7 @@ Returns:
 """
 ghenv.Component.Name = "Ladybug_Psychrometric Chart"
 ghenv.Component.NickName = 'PsychChart'
-ghenv.Component.Message = 'VER 0.0.62\nJAN_26_2016'
+ghenv.Component.Message = 'VER 0.0.62\nFEB_19_2016'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Ladybug"
 ghenv.Component.SubCategory = "2 | VisualizeWeatherData"
@@ -1770,7 +1770,7 @@ def main(epwData, epwStr, calcLength, airTemp, relHumid, barPress, avgBarPress, 
                     barPress2.append(barPress[0])
                 barPress = barPress2
             if len(patternList) == 8760:
-                HOYS, months, days = getHOYsBasedOnPeriod(analysisPeriod_, 1)
+                HOYS, months, days = lb_preparation.getHOYsBasedOnPeriod(analysisPeriod_, 1)
                 newPatternList = []
                 for hour in HOYS:
                     newPatternList.append(patternList[hour-1])
