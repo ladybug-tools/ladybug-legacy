62dc37a4f1bb87068f3897042c46b963c35df29d
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-05-11 08:35:13 -0400

    Fixed bug with legendParameters

--- src/Ladybug_Bioclimatic Chart.py
+++ src/Ladybug_Bioclimatic Chart.py
@@ -76,7 +76,7 @@ Provided by Ladybug 0.0.62
 """
 ghenv.Component.Name = "Ladybug_Bioclimatic Chart"
 ghenv.Component.NickName = 'Bioclimatic Chart'
-ghenv.Component.Message = 'VER 0.0.62\nJAN_26_2016'
+ghenv.Component.Message = 'VER 0.0.62\nMAY_11_2016'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Ladybug"
 #ghenv.Component.SubCategory = "2 | VisualizeWeatherData"
@@ -1330,7 +1330,8 @@ def main(epwData, epwStr):
         
         # Read the legend parameters.
         lowB, highB, numSeg, customColors, legendBasePoint, legendScale, \
-        legendFont, legendFontSize, legendBold = lb_preparation.readLegendParameters(legendPar_, False)
+        legendFont, legendFontSize, legendBold, decimalPlaces, \
+        removeLessThan= lb_preparation.readLegendParameters(legendPar_, False)
         
         if cullMesh_:
             cullMesh = cullMesh_
