cd9abdac84186da75aac0359a1af3d44010c8902
Chris Mackey <chris@mackeyarchitecture.com>
2016-06-26 19:24:02 -0400

    Updated Checks for HR Calculator

--- src/Ladybug_Humidity Ratio Calculator.py
+++ src/Ladybug_Humidity Ratio Calculator.py
@@ -47,7 +47,7 @@ Provided by Ladybug 0.0.62
 
 ghenv.Component.Name = "Ladybug_Humidity Ratio Calculator"
 ghenv.Component.NickName = 'CalcHumidityRatio'
-ghenv.Component.Message = 'VER 0.0.62\nJAN_26_2016'
+ghenv.Component.Message = 'VER 0.0.62\nJUN_26_2016'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Ladybug"
 ghenv.Component.SubCategory = "1 | AnalyzeWeatherData"
@@ -60,26 +60,140 @@ import math
 import scriptcontext as sc
 
 def checkTheData():
-    try:
-       hourlyDBTemp = _dryBulbTemperature
-       if 'Temperature' in hourlyDBTemp[2] and hourlyDBTemp[4] == 'Hourly': checkData1 = True
-       else: checkData1 = False
-       
-       hourlyRH = _relativeHumidity
-       if 'Relative Humidity' in hourlyRH[2] and hourlyRH[4] == 'Hourly': checkData2 = True
-       else: checkData2 = False
-       
-       barPress = _barometricPressure
-       if 'Barometric Pressure' in barPress[2] and barPress[4] == 'Hourly': checkData3 = True
-       else: checkData3 = False
-       
-       if checkData1 == True and checkData2 == True and checkData3 == True: checkData = True
-    except: checkData = False
+    #Define a value that will indicate whether someone has hooked up epw data.
+    epwData = False
+    epwStr = []
     
-    return checkData
+    #Check lenth of the _dryBulbTemperature list and evaluate the contents.
+    checkData1 = False
+    airTemp = []
+    airMultVal = False
+    if len(_dryBulbTemperature) != 0:
+        try:
+            if "Temperature" in _dryBulbTemperature[2]:
+                airTemp = _dryBulbTemperature[7:]
+                checkData1 = True
+                epwData = True
+                epwStr = _dryBulbTemperature[0:7]
+        except: pass
+        if checkData1 == False:
+            for item in _dryBulbTemperature:
+                try:
+                    airTemp.append(float(item))
+                    checkData1 = True
+                except: checkData1 = False
+        if len(airTemp) > 1: airMultVal = True
+        if checkData1 == False:
+            warning = '_dryBulbTemperature input does not contain valid temperature values in degrees Celcius.'
+            print warning
+            ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, warning)
+    else:
+        print 'Connect a temperature in degrees celcius for _dryBulbTemperature'
+    
+    #Check lenth of the _relativeHumidity list and evaluate the contents.
+    checkData4 = False
+    relHumid = []
+    humidMultVal = False
+    nonValue = True
+    if len(_relativeHumidity) != 0:
+        try:
+            if "Humidity" in _relativeHumidity[2]:
+                relHumid = _relativeHumidity[7:]
+                checkData4 = True
+                epwData = True
+                epwStr = _relativeHumidity[0:7]
+        except: pass
+        if checkData4 == False:
+            for item in _relativeHumidity:
+                try:
+                    if 0 <= float(item) <= 100:
+                        relHumid.append(float(item))
+                        checkData4 = True
+                    else: nonValue = False
+                except:checkData4 = False
+        if nonValue == False: checkData4 = False
+        if len(relHumid) > 1: humidMultVal = True
+        if checkData4 == False:
+            warning = '_relativeHumidity input does not contain valid value.'
+            print warning
+            ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, warning)
+    else:
+        print 'Connect a value for _relativeHumidity.'
+    
+    #Check lenth of the barometricPressure_ list and evaluate the contents.
+    checkData3 = False
+    barPress = []
+    pressMultVal = False
+    nonPositive = True
+    if len(barometricPressure_) != 0:
+        try:
+            if barometricPressure_[2] == 'Wind Speed':
+                barPress = barometricPressure_[7:]
+                checkData3 = True
+                epwData = True
+                epwStr = barometricPressure_[0:7]
+        except: pass
+        if checkData3 == False:
+            for item in barometricPressure_:
+                try:
+                    if float(item) >= 0:
+                        barPress.append(float(item))
+                        checkData3 = True
+                    else: nonPositive = False
+                except: checkData3 = False
+        if nonPositive == False: checkData3 = False
+        if len(barPress) > 1: pressMultVal = True
+        if checkData3 == False:
+            warning = 'barometricPressure_ input does not contain valid wind speed in meters per second.  Note that wind speed must be positive.'
+            print warning
+            ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, warning)
+    else:
+        checkData3 = True
+        barPress = [0.05]
+        print 'No value connected for barometricPressure_.  It will be assumed that the pressure is at seal level.'
+    
+    
+    #Finally, for those lists of length greater than 1, check to make sure that they are all the same length.
+    checkData2 = False
+    if checkData1 == True and checkData3 == True and checkData4 == True:
+        if airMultVal == True or pressMultVal == True or humidMultVal == True:
+            listLenCheck = []
+            if airMultVal == True: listLenCheck.append(len(airTemp))
+            if pressMultVal == True: listLenCheck.append(len(barPress))
+            if humidMultVal == True: listLenCheck.append(len(relHumid))
+            
+            if all(x == listLenCheck[0] for x in listLenCheck) == True:
+                checkData2 = True
+                calcLength = listLenCheck[0]
+                
+                def duplicateData(data, calcLength):
+                    dupData = []
+                    for count in range(calcLength):
+                        dupData.append(data[0])
+                    return dupData
+                
+                if airMultVal == False: airTemp = duplicateData(airTemp, calcLength)
+                if humidMultVal == False: relHumid = duplicateData(relHumid, calcLength)
+                if pressMultVal == False: barPress = duplicateData(barPress, calcLength)
+            else:
+                calcLength = None
+                warning = 'If you have put in lists with multiple values, the lengths of these lists must match across the parameters or you have a single value for a given parameter to be applied to all values in the list.'
+                print warning
+                ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, warning)
+        else:
+            checkData2 = True
+            calcLength = 1
+    else:
+        calcLength = 0
+    
+    checkData = False
+    if checkData1 == True and checkData2 == True and checkData3 == True and checkData4 == True: checkData = True
+    
+    return checkData, airTemp, relHumid, barPress, epwStr
 
 
-def main():
+
+def main(Tnumbers, Rnumbers, Bnumbers, Tstr):
     # import the classes
     if sc.sticky.has_key('ladybug_release'):
         try:
@@ -96,65 +210,47 @@ def main():
             
         lb_comfortModels = sc.sticky["ladybug_ComfortModels"]()
         
-        #Separate the numbers from the header strings
-        Tnumbers = []
-        Tstr = []
-        for item in _dryBulbTemperature:
-           try: Tnumbers.append(float(item))
-           except: Tstr.append(item)
-        
-        Rnumbers = []
-        Rstr = []
-        for item in _relativeHumidity:
-           try: Rnumbers.append(float(item))
-           except: Rstr.append(item)
-        
-        Bnumbers = []
-        Bstr = []
-        for item in _barometricPressure:
-           try: Bnumbers.append(float(item))
-           except: Bstr.append(item)
-        
         #Calculate the Humidity Ratio.
         HRCalc, ENCalc, vapPress, satPress = lb_comfortModels.calcHumidRatio(Tnumbers, Rnumbers, Bnumbers)
         
         #Build the strings and add it to the final calculation outputs
         HR = []
         
-        HR.append(Tstr[0])
-        HR.append(Tstr[1])
-        HR.append('Humidity Ratio')
-        HR.append('kg water / kg air')
-        HR.append(Tstr[4])
-        HR.append(Tstr[5])
-        HR.append(Tstr[6])
+        if Tstr != []:
+            HR.append(Tstr[0])
+            HR.append(Tstr[1])
+            HR.append('Humidity Ratio')
+            HR.append('kg water / kg air')
+            HR.append(Tstr[4])
+            HR.append(Tstr[5])
+            HR.append(Tstr[6])
         
         for item in HRCalc:
            HR.append(item)
         
         EN = []
-        
-        EN.append(Tstr[0])
-        EN.append(Tstr[1])
-        EN.append('Enthalpy')
-        EN.append('kJ/kg')
-        EN.append(Tstr[4])
-        EN.append(Tstr[5])
-        EN.append(Tstr[6])
+        if Tstr != []:
+            EN.append(Tstr[0])
+            EN.append(Tstr[1])
+            EN.append('Enthalpy')
+            EN.append('kJ/kg')
+            EN.append(Tstr[4])
+            EN.append(Tstr[5])
+            EN.append(Tstr[6])
         
         for item in ENCalc:
            EN.append(item)
         
         
         SP = []
-        
-        SP.append(Tstr[0])
-        SP.append(Tstr[1])
-        SP.append('Saturation Pressure')
-        SP.append('Pa')
-        SP.append(Tstr[4])
-        SP.append(Tstr[5])
-        SP.append(Tstr[6])
+        if Tstr != []:
+            SP.append(Tstr[0])
+            SP.append(Tstr[1])
+            SP.append('Saturation Pressure')
+            SP.append('Pa')
+            SP.append(Tstr[4])
+            SP.append(Tstr[5])
+            SP.append(Tstr[6])
         
         satPress100 = []
         for item in satPress:
@@ -164,14 +260,14 @@ def main():
            SP.append(item)
         
         VP = []
-        
-        VP.append(Tstr[0])
-        VP.append(Tstr[1])
-        VP.append('Vapor Pressure')
-        VP.append('Pa')
-        VP.append('Hourly')
-        VP.append(Tstr[5])
-        VP.append(Tstr[6])
+        if Tstr != []:
+            VP.append(Tstr[0])
+            VP.append(Tstr[1])
+            VP.append('Vapor Pressure')
+            VP.append('Pa')
+            VP.append('Hourly')
+            VP.append(Tstr[5])
+            VP.append(Tstr[6])
         
         vapPress100 = []
         for item in vapPress:
@@ -189,10 +285,10 @@ def main():
 
 
 #Check the data to make sure it is the correct type
-checkData = checkTheData()
+checkData, airTemp, relHumid, barPress, epwStr = checkTheData()
 
 if checkData == True:
-    res = main()
+    res = main(airTemp, relHumid, barPress, epwStr)
     if res!=-1:
         humidityRatio, enthalpy, partialPressure, saturationPressure = res
         print 'Humidity ratio calculation completed successfully!'
