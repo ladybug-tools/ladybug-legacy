41d4fe82bf547aedb839c22969b414070f86764e
AntonelloDN <antonellodinunzio@gmail.com>
2016-09-01 00:40:53 +0200

    Update - LB terrain generator

--- src/Ladybug_Terrain Generator.py
+++ src/Ladybug_Terrain Generator.py
@@ -28,7 +28,7 @@ This component requires an internet connection and it runs for free up to 2,500 
 Note that each surface is a request, for example if you use a surface made by sub-surfaces 6x6, this will be 36 requests.
 For informations about the rules of use of Google Maps API, take a look at this link:
 https://developers.google.com/maps/pricing-and-plans/#details
-
+-
 Special thanks goes to Google Maps and the authors of gHowl.
 -
 Provided by Ladybug 0.0.63
@@ -38,12 +38,11 @@ Provided by Ladybug 0.0.63
         _basePointGeo: Write latitude, longitude and elevation that represent WSG84 coordinates of the base point. You can achieve these type of coordinates from Google Maps or similar.
         -
         e.g. 40.821796, 14.426439, 990
-        _surface: Connect multiple surfaces. Remember that each surface is a request and you have 2500 requests per day.
+        _radius_: A radius to make the terrain 3D model in Rhino model units. The default is set to 100.
         -
-        I suggest you start from a rectangular surface and divide it by using "Isotrim" component, in this way it is possible to control the dimension of the sub-surfaces.
-        -
-        The output will have the same unit of your Rhino file. Thus the scale factor is 1:1; e.g. if your model is made in meters the outputs will be in meters.
-        _numDivision_: Set the number of points for each surface. If no input is connected this will be 15 (grid: 16x16).
+        If you provide a big radius, this could require lots of time (also a couple of minutes).
+        _numOfTiles_: Set the number of tiles (e.g. 4, that means 4x4). If no input is connected this will be 3 (tiles: 3x3).
+        _numDivision_: Set the number of points for each tile. If no input is connected this will be 15 (grid: 16x16).
         _imgResolution_: Connect an integer number which manage the quality of single satellite image.
         -
         The following list shows the approximate level of detail you can expect to see at each _imgResolution_ level:
@@ -60,12 +59,15 @@ Provided by Ladybug 0.0.63
         pointsGeo : WSG84 coordinates of the grid points (longitude, latitude).
         pointsXY : Cartesian coordinates of the grid points (X, Y).
         pointsZ : Z values of the grid points. Connect this output to a Z vector to move the pointsXY in the right positions.
+        tiles: The area which will be calculated. If you want to visualize Satellite images connect this output to 'G' input of 'Human Custom Preview Material'.
         imagePath: Satellite images from Google Static Maps API. Connect it to 'DB' input of 'Human Custom Preview Material' to apply textures to the 3d model or to the list of input surfaces.
+        -----------: ...
+        terrain: 3D terrain model.
 """
 
 ghenv.Component.Name = "Ladybug_Terrain Generator"
 ghenv.Component.NickName = 'TerrainGenerator'
-ghenv.Component.Message = 'VER 0.0.63\nAUG_19_2016'
+ghenv.Component.Message = 'VER 0.0.63\nAUG_30_2016'
 ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
 ghenv.Component.Category = "Ladybug"
 ghenv.Component.SubCategory = "7 | WIP"
@@ -80,7 +82,7 @@ import socket
 import System
 import os
 import clr
-from math import pi, log, tan, atan, exp
+from math import pi, log, tan, atan, exp, sqrt
 
 clr.AddReference("Grasshopper")
 import Grasshopper.Kernel as gh
@@ -88,10 +90,10 @@ from Grasshopper.Kernel.Data import GH_Path
 from Grasshopper import DataTree
 
 
-def checkInputs(basePoint, basePointGeo, surface):
-    if basePoint == None or basePointGeo == None or surface == None:
+def checkInputs(basePoint, basePointGeo):
+    if basePoint == None or basePointGeo == None:
         return False
-    elif basePoint and basePointGeo and surface:
+    elif basePoint and basePointGeo:
         return True
 
 
@@ -133,7 +135,6 @@ def divideSrf(srf, numDivision):
 
 
 def terrainGen(pts, xf, run):
-    
     if len(pts) > 257:
         warning = 'Your request is too big. The upper limit of _numDivision_ is 15.'
         ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, warning)
@@ -146,7 +147,7 @@ def terrainGen(pts, xf, run):
     flat_list_coordinate = flat_list_coordinate[:len(flat_list_coordinate)-1]
     url_part2 = flat_list_coordinate
     
-    url_part1 = 'https://maps.googleapis.com/maps/api/elevation/json?locations='
+    url_part1 = 'http://maps.googleapis.com/maps/api/elevation/json?locations='
     goog_url = url_part1 + url_part2
     
     # Temporary file
@@ -156,8 +157,11 @@ def terrainGen(pts, xf, run):
     name = os.path.join(appdata, "Ladybug\\", "elevation.txt")
     if len(pts) <= 257:
         if run:
-            client = System.Net.WebClient()
-            written_file = client.DownloadFile(goog_url, name)
+            try:
+                client = System.Net.WebClient()
+                written_file = client.DownloadFile(goog_url, name)
+            except:
+                pass
     
     open_file = open(name, 'r')
     txt = open_file.read()
@@ -175,7 +179,7 @@ def terrainGen(pts, xf, run):
         num = float(num)
         elevations.append(num)
     
-    return points, elevations, pts
+    return points, elevations
 
 
 def centerPtsGeo(srf):
@@ -190,8 +194,8 @@ def findCorner(relation, list_latitude, list_longitude):
     relation_longitude = relation(list_longitude)
     indices_longitude_relation = {i for i, elem in enumerate(list_longitude) if elem == relation_longitude}
     
-    a = indices_latitude_relation.intersection(indices_longitude_relation)
-    value = list(a)[0]
+    intersection = indices_latitude_relation.intersection(indices_longitude_relation)
+    value = list(intersection)[0]
     
     return value
 
@@ -228,7 +232,7 @@ def textureGen(run, center, points, origin_shift, initial_resolution, imgResolut
         ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, warning)
         return -1
     
-    urlPart1 = 'https://maps.googleapis.com/maps/api/staticmap?'
+    urlPart1 = 'http://maps.googleapis.com/maps/api/staticmap?'
     urlPart2 = "center={0},%20{1}&zoom={2}&size={3}x{4}&maptype=satellite".format(str(center.Y), str(center.X), str(zoom), str(int(dx)), str(int(dy)))
     
     goog_url = urlPart1 + urlPart2
@@ -236,8 +240,41 @@ def textureGen(run, center, points, origin_shift, initial_resolution, imgResolut
     return goog_url
 
 
-def main():
+def createTiles(base_point, radius, numOfTiles):
+    interval_radius = Rhino.Geometry.Interval(-radius, radius)
+    rectangle_tiles = Rhino.Geometry.Rectangle3d(Rhino.Geometry.Plane(base_point, Rhino.Geometry.Vector3d.ZAxis), interval_radius, interval_radius).ToNurbsCurve()
+    brep_tiles = Rhino.Geometry.Brep.CreatePlanarBreps(rectangle_tiles)[0]
+    surface_tiles = brep_tiles.Surfaces[0]
+    surface_tiles.SetDomain(0, Rhino.Geometry.Interval(0, 1))
+    surface_tiles.SetDomain(1, Rhino.Geometry.Interval(0, 1))
     
+    edge = brep_tiles.DuplicateEdgeCurves(True)[0]
+    t = edge.DivideByCount(numOfTiles, True)
+    
+    intervals = []
+    for i in range(0, numOfTiles):
+        part = Rhino.Geometry.Interval(t[i], t[i+1])
+        intervals.append(part)
+    
+    tiles = []
+    for part in intervals:
+        for part2 in intervals:
+            tiles.append(surface_tiles.Trim(part, part2))
+    
+    return tiles
+
+
+def cullAndSortPoints(pts, elevations):
+    terrain_pts = [Rhino.Geometry.Point3d(pt.X, pt.Y, elevations[i]) for i, pt in enumerate(pts)]
+    cull_pts = list(set(terrain_pts))
+    
+    cull_pts.sort(key = lambda pt: pt.X)
+    cull_pts.sort(key = lambda pt: pt.Y, reverse=True)
+    
+    return cull_pts
+
+
+def main():
     earth_radius = 6378137
     equator_circumference = 2 * pi * earth_radius
     initial_resolution = equator_circumference / 256.0
@@ -248,22 +285,36 @@ def main():
     if _numDivision_ == None:
         numDivision = 15
     else: numDivision = int(_numDivision_)
+    if _radius_ == None:
+        radius = 100
+    else: radius = _radius_
+    if _numOfTiles_ == None:
+        numOfTiles = 3
+    else: numOfTiles = int(_numOfTiles_)
     
+    xf = earthPoint(_basePointGeo, _basePoint) 
     
+    tilesTree = DataTree[System.Object]()
+    tiles = createTiles(_basePoint, radius, numOfTiles)
+    for i, tile in enumerate(tiles):
+        path = GH_Path(0, i)
+        tilesTree.Add(tile, path)
     
-    xf = earthPoint(_basePointGeo, _basePoint)
-    
+    points_for_srf = []
+    elevations_for_srf = []
     URLs = []
     imagePath = DataTree[System.Object]()
     if _runIt:
         pointsGeo, pointsZ, pointsXY, imagePath  = DataTree[System.Object](), DataTree[System.Object](), DataTree[System.Object](), DataTree[System.Object]()
         
-        for i in range(len(_surface)):
-            points_srf = divideSrf(_surface[i], numDivision)
+        for i in range(len(tiles)):
+            points_srf = divideSrf(tiles[i], numDivision)
             try:
-                points, elevations, pts = terrainGen(points_srf, xf, _runIt)
-                ptCenter = centerPtsGeo(_surface[i])
+                points, elevations = terrainGen(points_srf, xf, _runIt)
+                ptCenter = centerPtsGeo(tiles[i])
                 pointGeo = xf * ptCenter
+                points_for_srf.extend(points_srf)
+                elevations_for_srf.extend(elevations)
                 
                 URL = textureGen(_runIt, pointGeo, points, origin_shift, initial_resolution, imgResolution)
                 URLs.append(URL)
@@ -271,9 +322,16 @@ def main():
                 path = GH_Path(0, i)
                 pointsGeo.AddRange(points, path)
                 pointsZ.AddRange(elevations, path)
-                pointsXY.AddRange(pts, path)
+                pointsXY.AddRange(points_srf, path)
                 
-            except TypeError: return None, None, None, None
+            except TypeError: return None, None, None, None, None, None
+        
+        # make 3D points
+        # thanks to djordje for this advice
+        cull_pts = cullAndSortPoints(points_for_srf, elevations_for_srf)
+        num = (numDivision + 1) * numOfTiles - (numOfTiles - 1)
+        lb_meshpreparation = sc.sticky["ladybug_Mesh"]()
+        terrainMesh = lb_meshpreparation.meshFromPoints(num, num, cull_pts)
         
         # make a folder for the images
         appdata = os.getenv("APPDATA")
@@ -290,11 +348,14 @@ def main():
         except:
             pass
             print("Something went wrong during the request of images. Please, try again.")
-        
-        
-    else: return None, None, None, None
     
-    return pointsGeo, pointsZ, pointsXY, imagePath
+    else:
+        # check the grid size
+        dimension = round(((radius * 2) / numOfTiles) / numDivision, 3)
+        print("Size of the grid = {0} x {0}".format(dimension))
+        return None, None, None, None, None, tilesTree
+    
+    return pointsGeo, pointsZ, pointsXY, imagePath, terrainMesh, tilesTree
 
 
 initCheck = False
@@ -318,12 +379,12 @@ else:
     ghenv.Component.AddRuntimeMessage(w, "You should first let the Ladybug fly...")
 
 
-check = checkInputs(_basePoint, _basePointGeo, _surface)
+check = checkInputs(_basePoint, _basePointGeo)
 if check and initCheck:
     if checkInternetConnection():
         result = main()
         if result != -1:
-            pointsGeo, pointsZ, pointsXY, imagePath = result
+            pointsGeo, pointsZ, pointsXY, imagePath, terrain, tiles = result
     else:
         warning = "Please enable your internet connection."
         ghenv.Component.AddRuntimeMessage(gh.GH_RuntimeMessageLevel.Warning, warning)
