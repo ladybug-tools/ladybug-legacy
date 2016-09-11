3c5dd912e897acd3cb2391094f652e91d8531450
Devang Chauhan <devngc@gmail.com>
2016-09-11 17:05:11 +0530

    Tips to turn into a shadow range component added

--- src/Ladybug_Shadow Study.py
+++ src/Ladybug_Shadow Study.py
@@ -23,6 +23,7 @@
 """
 Use this component to generate outline curves representing shadows cast by input _geometry for a given _sunVector.
 Note that, to see shadows cast onto a ground, a surface representing the ground plane must be included in the input _geometry.
+Connect output of Ladybug_Analysis period component to analysisPeriod_ on Ladybug_SunPath component. This will let you use a range of sunvectors. Using these range of sunvectors, you can turn this shadow study into a shadow range study.
 Also, please note that, for a list of input _geometry that is larger than 4 or 5 breps, the calculation time of this component can be very long.  Please keep the input geometry to small lists or be prepared to wait a long time.
 WARNING: This component is a proof of concept that will not work in every situation.  It is not ideal for analyzing curved surfaces and it is not able to calculate shadows for geometries that are intersecting each other.
 -
