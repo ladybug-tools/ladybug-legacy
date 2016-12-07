0e49adae5cc169cea7476b0c3629be6d7392fab8
Devang <devang@outlook.in>
2016-12-07 18:41:53 +0530

    Wind Rose Minor Update
    Legend will only shifted to the right if both frequencies and average
    velocities are turned on by the user. Else, it will remain at the
    default location.

--- src/Ladybug_Wind Rose.py
+++ src/Ladybug_Wind Rose.py
@@ -742,9 +742,14 @@ def main(north, hourlyWindDirection, hourlyWindSpeed, annualHourlyData,
                             calmValues.append(selList[h])
                             allValues.append(selList[h])
 
+                        # If the user has asked to see average velocities and frequencies both, we shall push the legend to the right a bit
+                        if showFrequency_ == True and showAverageVelocity_ == True:
+                            lb_visualization.BoundingBoxPar[0].X = lb_visualization.BoundingBoxPar[0].X * 1.1
+                        # Else, let it be the way it is
+                        else:
+                            lb_visualization.BoundingBoxPar[0].X = lb_visualization.BoundingBoxPar[0].X * 1
+                        
                         # get the legend done
-                        # This moves the legend to the right
-                        lb_visualization.BoundingBoxPar[0].X = lb_visualization.BoundingBoxPar[0].X * 1.1
                         legendSrfs, legendText, legendTextCrv, textPt, textSize = lb_visualization.createLegend(allValues
                                 , lowB, highB, numSeg, listInfo[i][3], lb_visualization.BoundingBoxPar, legendBasePoint, legendScale, legendFont, legendFontSize, legendBold, decimalPlaces, removeLessThan)
 
