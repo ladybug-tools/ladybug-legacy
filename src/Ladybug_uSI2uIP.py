a2a8bf42aa115be8730e00ea1c1c5f9733e34746
Chris Mackey <chris@mackeyarchitecture.com>
2017-08-20 13:08:44 -0400

    Converter for U-Values

---
+++ src/Ladybug_uSI2uIP.py
@@ -0,0 +1,48 @@
+#
+# Ladybug: A Plugin for Environmental Analysis (GPL) started by Mostapha Sadeghipour Roudsari
+# 
+# This file is part of Ladybug.
+# 
+# Copyright (c) 2013-2017, Chris Mackey <Chris@MackeyArchitecture.com> 
+# Ladybug is free software; you can redistribute it and/or modify 
+# it under the terms of the GNU General Public License as published 
+# by the Free Software Foundation; either version 3 of the License, 
+# or (at your option) any later version. 
+# 
+# Ladybug is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Ladybug; If not, see <http://www.gnu.org/licenses/>.
+# 
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+
+
+"""
+Use this component to convert U-Values in SI (W/Km2) to U-Values in IP (BTU/hft2F).
+-
+Provided by Ladybug 0.0.65
+    
+    Args:
+        _U_SI: A U-Value in SI (W/Km2).
+    Returns:
+        U_IP: The U-Value in IP (BTU/hft2F).
+"""
+
+ghenv.Component.Name = "Ladybug_uSI2uIP"
+ghenv.Component.NickName = 'uSI2uIP'
+ghenv.Component.Message = 'VER 0.0.65\nAUG_20_2017'
+ghenv.Component.IconDisplayMode = ghenv.Component.IconDisplayMode.application
+ghenv.Component.Category = "Ladybug"
+ghenv.Component.SubCategory = "5 | Extra"
+#compatibleLBVersion = VER 0.0.59\nFEB_01_2015
+try: ghenv.Component.AdditionalHelpFromDocStrings = "0"
+except: pass
+
+
+U_IP = []
+for num in _U_SI:
+    try: U_IP.append(float(num)/5.678263337)
+    except: U_IP.append(num)
