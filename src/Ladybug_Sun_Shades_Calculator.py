bac588b1e4375156ea2cc0c24a046fef38d3fb57
ayezioro <ayezioro@gmail.com>
2018-01-18 08:00:35 +0200

    New try to commit

--- src/Ladybug_Sun_Shades_Calculator.py
+++ src/Ladybug_Sun_Shades_Calculator.py
@@ -7,7 +7,7 @@
 # 
 # Sun Shades Calculator is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of 
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 # 
 # See <http://www.gnu.org/licenses/>.
